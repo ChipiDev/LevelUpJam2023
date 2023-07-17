9a507920c63b8c95c67550fcd187a162c8e70856
Chipi <josemariaborrella@gmail.com>
2023-07-17 20:05:30 +0200

    pers.1: Prueba de subir y mezclar codigo

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,7 +7,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Prueba");
     }
 
     // Update is called once per frame
