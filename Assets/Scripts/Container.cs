396a66f5fe8bd88cffe1eb979b80b634d29d9417
Chipi <josemariaborrella@gmail.com>
2023-07-20 17:20:55 +0200

    Añadido elementos básicos del primer escenario

---
+++ Assets/Scripts/Container.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Trash;
+
+public class Container : MonoBehaviour
+{
+    public ETrashType type;
+    private bool isMouseInside = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isMouseInside && Trash.pickedTrash)
+        {
+            if (!Input.GetMouseButton(0))
+            {
+                if (type == Trash.pickedTrash.type)
+                {
+                    PickUpTrash(Trash.pickedTrash);
+                    Trash.pickedTrash = null;
+                }else
+                {
+                    // Que se ha equivocao
+                    Trash.pickedTrash.collider2Dcollider.enabled = true;
+                    Trash.pickedTrash.Restore();
+                    Trash.pickedTrash = null;
+                }
+            }
+        }
+    }
+
+    private void OnMouseEnter()
+    {
+        isMouseInside = true;
+    }
+
+    private void OnMouseExit()
+    {
+        isMouseInside = false;
+    }
+
+    private void PickUpTrash(Trash trash)
+    {
+        Destroy(trash.gameObject);
+    }
+}
