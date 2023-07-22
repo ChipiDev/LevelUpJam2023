c9a87ef985676e10f9f613c16c5f08b9f97b400f
eldreco <andreffcd@gmail.com>
2023-07-22 11:04:13 +0100

    Solo permitir reutilizar con mecanica activa

--- Assets/Scripts/Reusable.cs
+++ Assets/Scripts/Reusable.cs
@@ -7,14 +7,17 @@ public class Reusable : MonoBehaviour
 
     //Objeto nuevo despues de clickar
     [SerializeField] private GameObject newBackgroundObject;
+    private GameManager gameManager;
 
     void Start()
     {
         gameObject.SetActive(true);
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     private void OnMouseDown() {
-        Pickup();
+        if(!gameManager.GetReciclarActive())
+            Pickup();
     }
 
     public void Pickup()
