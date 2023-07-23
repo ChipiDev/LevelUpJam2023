6d08afb44560540b0ae15be74863461a1c13692c
Chipi <josemariaborrella@gmail.com>
2023-07-23 10:13:52 +0200

    Personaje y avance arreglo de dialogo

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -8,6 +8,14 @@ using UnityEngine.SceneManagement;
 public class Dialogue : MonoBehaviour
 {
     public string allTexts;
+    public string incorrectMessage;
+    public string trashOnCoco;
+    public string incorrectTrashMechanic;
+    public string incorrectReusableMechanic;
+    public string cocoClick;
+    public string cocoClick2;
+    public string completationMessage;
+
     [SerializeField] private GameObject box;
     [SerializeField] private TMP_Text text;
 
@@ -45,16 +53,19 @@ public class Dialogue : MonoBehaviour
     }
 
     // Start is called before the first frame update
-    void Start()
+    IEnumerator Start()
     {
         box.SetActive(false);
         gameObject.GetComponent<Collider2D>().enabled = false;
         isActive = false;
         isFinal = false;
 
+        //* Esperamos un fotograma para que los textos se actualicen
+        yield return null;
         Debug.Log(allTexts);
         textList = allTexts.Split('|');
 
+        transform.parent = Camera.main.transform;
     }
 
     private void OnMouseDown() {
